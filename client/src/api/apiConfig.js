ad58416c0daa176d1206e64efa61ee6e3a139696
= <86667626+ibrahim-atef@users.noreply.github.com>
2024-09-03 01:51:39 +0300

    cancel merge with main b

--- client/src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-  apiKey: "537b2b4f531f7f24cf82bbac9260de77",
-  baseUrl: "https://api.themoviedb.org/3/",
-  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
-};
-
-export default apiConfig;
