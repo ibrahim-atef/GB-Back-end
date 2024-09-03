4df9efe9660441f2dbb0e6fb0a4a0146242f57bc
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-03 21:21:32 +0300

    Fixing Bugs in client side

---
+++ client/src/pages/Catalog.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import PageHeader from "../coponents/utilitiesCpmponents/page-header/PageHeader";
+import { category as cate } from "../api/tmdbApi";
+import MovieGrid from "../coponents/utilitiesCpmponents/movie-grid/MovieGrid";
+
+const Catalog = () => {
+  const { category } = useParams();
+  console.log(category);
+  return (
+    <>
+      <PageHeader>
+        {category === cate.movie ? "Movies" : "TV Series"}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Catalog;
