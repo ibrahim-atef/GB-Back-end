a117e6225555bec7cf86b15464bf90eed4d66122
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-04 19:12:23 +0300

    Dockerized Version

--- api/assets/SQLDB/initDB.js
+++ api/assets/SQLDB/initDB.js
@@ -1,16 +1,19 @@
-const sequelize = require("./db");
+const { sqlDB, createDatabaseIfNotExists, tempSequelize } = require("./db");
 const User = require("../../models/User");
-
 
 const initDB = async () => {
     try {
-        await sequelize.authenticate(); // Test the connection to the database
+        // Create the database if it does not exist
+        await createDatabaseIfNotExists(tempSequelize);
+
+        // Authenticate with the correct database
+        await sqlDB.authenticate(); // Test the connection to the database
         console.log("Connected to MySQL database.");
-        
+
         // Sync models with the database
-        await sequelize.sync({ alter: true }); // Set to `true` to drop tables and recreate them
+        await sqlDB.sync({ alter: true }); // `alter: true` will modify the table to match the model
         console.log("MySQL Database synchronized.");
-        
+
         // Add any other initialization code here
     } catch (error) {
         console.error("Unable to connect to MySQL database:", error);
